04e35fef5fbab514c3f3cc5635427180114bbb8f
Thijs Metsch <tmetsch@openindy.(none)>
2011-08-24 14:35:42 +0200

    minor changes

--- cluster_info.py
+++ cluster_info.py
@@ -2,9 +2,11 @@
 
 from pylsf import lsf
 
-print '\n Hosts in cluster: ', lsf.get_host_names(), '\n'
+print '\n Hosts in cluster: ', lsf.get_host_names()
 
-print '{0:15s} {1:10s} {2:10s} {3:5s} {4:4s}'.format('Hostname', 'Type',
+print '\n Clustername: ', lsf.ls_getclustername(), '\n'
+
+print '{0:15s} {1:20s} {2:20s} {3:5s} {4:4s}'.format('Hostname', 'Type',
                                                      'Model', 'Cores', 'Load')
 
 for info in lsf.get_host_info():
@@ -13,11 +15,11 @@ for info in lsf.get_host_info():
     if load >= 65535:
         load = -1
 
-    print '{0:15s} {1:10s} {2:10s} {3:5d} {4:4.2f}'.format(info.hostName,
+    print '{0:15s} {1:20s} {2:20s} {3:5d} {4:4.2f}'.format(info.hostName,
                                                            info.hostType,
                                                            info.hostModel,
                                                            info.cores,
                                                            load)
 
     if info.nRes > 0:
-        print '+--> Resources:', info.resources
+        print ' +--> Resources:', info.resources
